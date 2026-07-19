c1ba7f67cd42f100ee86a53b29c91ab332ce766f
Khanh Vu <khanhvn06.h.2124@gmail.com>
2026-07-19 08:23:52 +0700

    add: payment record

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -2,4 +2,8 @@ import { api } from "./api";
 
 export const paymentService = {
   getAllPayments: () => api.get("/payments"),
+  getInvoiceDetail: (invoiceId) => api.get(`/payments/invoices/${invoiceId}`),
+  getUnpaidInvoices: () => api.get("/payments/unpaid-invoices"),
+  getPaymentDetail: (paymentId) => api.get(`/payments/${paymentId}`),
+  payInvoice: (invoiceId, paymentMethod) => api.post(`/payments/invoices/${invoiceId}/pay`, { paymentMethod }),
 };
